53adec576500b5060c7fb7e6ae82c1f0eb7ead0f
jboy7977 <56408797+jboy7977@users.noreply.github.com>
2019-11-22 09:46:17 -0700

    hihhi

---
+++ github_Testing/Controllers/YesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace github_Testing.Controllers
+{
+    public class YesController : Controller
+    {
+        // GET: Yes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
